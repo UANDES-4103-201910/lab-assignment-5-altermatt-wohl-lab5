38d868d4c2d5b86ae1ad42015b2decf939c6cee7
nozeback <ajaltermatt@miuandes.cl>
2019-04-10 10:42:08 -0400

    events semi

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,17 @@
 class EventsController < ApplicationController
+	def show
+	
+	end
+	def create 
+	 evento=Event.new(params[:event])
+	 evento.save!
+	end
+	def update
+	 evento = event.find(params[:name])
+	 evento.update!(event_params)
+	end
+	def destroy
+	 event = current_account.event.find(params[:name])
+	 event.destroy!
+	end
 end
