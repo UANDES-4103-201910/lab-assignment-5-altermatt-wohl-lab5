fc4f738c35b5f03dcc2e5fd0a60af33ee5bc1e91
nozeback <ajaltermatt@miuandes.cl>
2019-04-10 11:09:05 -0400

    show json events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,17 +1,18 @@
 class EventsController < ApplicationController
 	def show
-	
+	 evento = Event.find(params[:id]) 
+     render json: evento
 	end
 	def create 
 	 evento=Event.new(params[:event])
 	 evento.save!
 	end
 	def update
-	 evento = event.find(params[:id])
+	 evento = Event.find(params[:id])
 	 evento.update!(event_params)
 	end
 	def destroy
-	 event = event.find(params[:id])
+	 event = Event.find(params[:id])
 	 event.destroy!
 	end
 end
