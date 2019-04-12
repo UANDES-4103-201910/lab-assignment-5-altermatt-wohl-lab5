02e7bc9ef19bf66b39da250b892382aeb6cb6c2a
nozeback <ajaltermatt@miuandes.cl>
2019-04-11 22:40:43 -0400

    algunos routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,23 @@ Rails.application.routes.draw do
         resources :tickets
         resources :tickettypes
         resources :users
-         (get) ticket_types/:ticket_types_id/tickets tickettypes#
+        get ticket_types/:ticket_types_id/tickets tickettypes#index
+        get users/:user_id/user_name users#show
+        get  users/:user_id/new users#create
+        delete users/:user_id/delete users#destroy
+        patch/put users/:user_id users#update
+        get  tickets/:ticket_id/new tickets#create
+        delete tickets/:ticket_id/delete tickets#destroy
+        patch/put tickets/:ticket_id tickets#update
+        get event_venues/:event_venues_id/new eventvenues#create
+        delete event_venues/:event_venues_id/delete eventvenues#destroy
+        patch/put event_venues/:event_venues_id eventvenues#update
+        get events/:event_id/new events#create
+        delete events/:event_id/delete events#destroy
+        patch/put events/:event_id events#update
+        get  tickettypes/:ticket_type_id/new tickettypes#create
+        delete tickettypes/:ticket_type_id/delete tickettypes#destroy
+        patch/put tickettypes/:ticket_type_id tickettypes#update
+        
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
